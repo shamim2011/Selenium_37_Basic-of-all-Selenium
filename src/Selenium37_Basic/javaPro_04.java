4510a3e478622936b129ce0105d2a8635f99c901
shamim2011 <akhtarmdsamim786@gmail.com>
2025-11-19 22:36:23 +0530

    findElement(By.name(q));

--- src/Selenium37_Basic/javaPro_04.java
+++ src/Selenium37_Basic/javaPro_04.java
@@ -1,6 +1,9 @@
 package Selenium37_Basic;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class javaPro_04 {
@@ -8,5 +11,9 @@ public class javaPro_04 {
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://iamsandesh23.github.io/selenium.github.io/");
 		driver.manage().window().maximize();
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Kodnest");
+		WebElement link = driver.findElement(By.linkText("onlytestingblog"));
+		link.clear();
 	}
 }
