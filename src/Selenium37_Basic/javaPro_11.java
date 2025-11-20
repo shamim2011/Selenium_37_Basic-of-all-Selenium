a193e143545c2426ebb5bb35d385b05ff0c7079a
shamim2011 <akhtarmdsamim786@gmail.com>
2025-11-20 22:35:40 +0530

    getWindowHandles() of multiple windows

---
+++ src/Selenium37_Basic/javaPro_11.java
@@ -0,0 +1,31 @@
+package Selenium37_Basic;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class javaPro_11 {
+
+	public static void main(String[] args) {
+		try {
+			WebDriver driver= new ChromeDriver();
+			driver.get("https://iamsandesh23.github.io/selenium.github.io/");
+			driver.manage().window().maximize();
+			
+			// getWindowHandles() - Handling the multiple windows
+			driver.findElement(By.linkText("Open a popup window")).click();
+			Set<String> windows = driver.getWindowHandles();
+			Iterator<String> itr = windows.iterator();
+			while(itr.hasNext()) {
+				System.out.println(itr.next());
+			}
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
