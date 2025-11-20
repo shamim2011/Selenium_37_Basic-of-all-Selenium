f74ad0f2a91e78a5d5f87839dc2d16c32979f4bd
shamim2011 <akhtarmdsamim786@gmail.com>
2025-11-20 15:44:23 +0530

    name and linkTest & click add

--- src/Selenium37_Basic/javaPro_04.java
+++ src/Selenium37_Basic/javaPro_04.java
@@ -8,13 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class javaPro_04 {
 	public static void main(String[] args) {
-		WebDriver driver= new ChromeDriver();
-		driver.get("https://iamsandesh23.github.io/selenium.github.io/");
-		driver.manage().window().maximize();
-		WebElement element = driver.findElement(By.name("q"));
-		element.sendKeys("Kodnest");
-		WebElement link = driver.findElement(By.linkText("onlytestingblog"));
-		link.click();
-		
+		try {
+			WebDriver driver= new ChromeDriver();
+			driver.get("https://iamsandesh23.github.io/selenium.github.io/");
+			driver.manage().window().maximize();
+//			WebElement element = driver.findElement(By.name("q"));
+//			Thread.sleep(5000);
+//			element.sendKeys("Kodnest");
+			WebElement link = driver.findElement(By.linkText("onlytestingblog"));
+			link.click(); 
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
