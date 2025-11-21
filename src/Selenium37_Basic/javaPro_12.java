b7abef4ac68c39226c91c7ac1ca5fea89716af15
shamim2011 <akhtarmdsamim786@gmail.com>
2025-11-21 11:52:03 +0530

    implicit wait

--- src/Selenium37_Basic/javaPro_12.java
+++ src/Selenium37_Basic/javaPro_12.java
@@ -1,38 +1,38 @@
 package Selenium37_Basic;
 
-import java.util.Iterator;
-import java.util.Set;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class javaPro_12 {
 
 	public static void main(String[] args) {
-		try {
-			WebDriver driver= new ChromeDriver();
-			driver.get("https://iamsandesh23.github.io/selenium.github.io/");
-			driver.manage().window().maximize();
-			
-			// getWindowHandles() - Handling the multiple windows
-			driver.findElement(By.linkText("Open a popup window")).click();
-			Set<String> windows = driver.getWindowHandles();
-			Iterator<String> itr = windows.iterator();
-			
-			// switchTo()
-			System.out.println("------------------------------------");
-			String mainWin = itr.next();
-			String childWin = itr.next();
-			driver.switchTo().window(childWin);
-			Thread.sleep(5000);
-			driver.switchTo().window(mainWin);
-			Thread.sleep(5000);
-			driver.quit();
-			
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
+//		try {
+//			WebDriver driver= new ChromeDriver();
+//			driver.get("https://iamsandesh23.github.io/selenium.github.io/");
+//			driver.manage().window().maximize();
+//			
+//			// Using Thread for waiting mechanism
+////			driver.findElement(By.className("dropbtn")).click();
+////			Thread.sleep(5000);
+////			driver.findElement(By.linkText("Facebook")).click();
+//		}
+//		catch(Exception e) {
+//			e.printStackTrace();
+//		}
+		
+		
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://iamsandesh23.github.io/selenium.github.io/");
+		driver.manage().window().maximize();
+		
+		/// implicit wait -  driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.className("dropbtn")).click();
+		driver.findElement(By.linkText("Facebook")).click();
+		System.out.println("------------------------------------");
 	}
 }
