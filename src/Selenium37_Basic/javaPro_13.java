4dcc148a26b74f6c7f73ad2263be85a9113f97b5
shamim2011 <akhtarmdsamim786@gmail.com>
2025-11-22 14:50:50 +0530

    13_drop-down method

--- src/Selenium37_Basic/javaPro_13.java
+++ src/Selenium37_Basic/javaPro_13.java
@@ -2,10 +2,11 @@ package Selenium37_Basic;
 
 import java.time.Duration;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class javaPro_13 {
 	public static void main(String[] args) throws InterruptedException{
@@ -13,14 +14,24 @@ public class javaPro_13 {
 		driver.get("https://iamsandesh23.github.io/selenium.github.io/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
 		/// Alert method
-		driver.findElement(By.id("alert1")).click();
-		Alert alert = driver.switchTo().alert();
-		String alertmsg = alert.getText();
-		System.out.println(alertmsg);
-		Thread.sleep(5000);
-		alert.accept();
-		driver.close();
+//		driver.findElement(By.id("alert1")).click();
+//		Alert alert = driver.switchTo().alert();
+//		String alertmsg = alert.getText();
+//		System.out.println(alertmsg);
+//		Thread.sleep(5000);
+//		alert.accept();
+//		driver.close();
+		
+		// Handling drop-down and Multi-Selection
+//		driver.findElement(By.id("drop1")).sendKeys("doc 3");
+		
+		WebElement dropdownField = driver.findElement(By.id("drop1"));
+		Select select = new Select(dropdownField);
+//		select.selectByVisibleText("doc 3");   // instead of sendKeys()
+//		select.selectByIndex(3);   // instead of sendKeys()
+//		select.deselectByVisibleText("doc3");
 		System.out.println("------------------------------------");
 
 	}
